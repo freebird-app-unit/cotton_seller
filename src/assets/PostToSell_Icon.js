dc802bc7134511bf7596ea9f4e1d20cae4dff497
Freebird <81305380+freebird-app-unit@users.noreply.github.com>
2021-11-25 13:27:32 +0530

    Icon changes commited - Bhavin

---
+++ src/assets/PostToSell_Icon.js
@@ -0,0 +1,67 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+function PostToSell_Icon(props) {
+  return (
+    <Svg
+      data-name="Group 11091"
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <Path data-name="Path 307" d="M0 0h24v24H0z" fill="none" />
+      <Path
+        data-name="Line 176"
+        transform="translate(3 21)"
+        fill="none"
+        stroke="#2c3e50"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+        d="M0 0L18 0"
+      />
+      <Path
+        data-name="Path 308"
+        d="M3 7v1a3 3 0 006 0V7m0 1a3 3 0 006 0V7m0 1a3 3 0 006 0V7H3l2-4h14l2 4"
+        fill="none"
+        stroke="#2c3e50"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+      />
+      <Path
+        data-name="Line 177"
+        transform="translate(5 10.85)"
+        fill="none"
+        stroke="#2c3e50"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+        d="M0 10.15L0 0"
+      />
+      <Path
+        data-name="Line 178"
+        transform="translate(19 10.85)"
+        fill="none"
+        stroke="#2c3e50"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+        d="M0 10.15L0 0"
+      />
+      <Path
+        data-name="Path 309"
+        d="M9 21v-4a2 2 0 012-2h2a2 2 0 012 2v4"
+        fill="none"
+        stroke="#69ba53"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+      />
+    </Svg>
+  )
+}
+
+export default PostToSell_Icon
