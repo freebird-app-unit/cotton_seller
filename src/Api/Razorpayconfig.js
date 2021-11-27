472fe5a41f87609896b469a35412601805ecfaeb
Freebird <81305380+freebird-app-unit@users.noreply.github.com>
2021-11-27 12:19:49 +0530

    Razorpay config - Bhavin

--- src/Api/Razorpayconfig.js
+++ src/Api/Razorpayconfig.js
@@ -1,2 +1,7 @@
-export const RazorpayApiKey = "rzp_test_U9XPrBYugyMmjF"
-export const RazorpayApiSecret  =  "EvpKmw4jR5WTPE9J4eqAD7cU"
+//Sandbox Detail
+// export const RazorpayApiKey = "rzp_test_U9XPrBYugyMmjF"
+// export const RazorpayApiSecret  =  "EvpKmw4jR5WTPE9J4eqAD7cU"
+
+//Live Detail
+export const RazorpayApiKey = "rzp_live_QVT4IA61g5Rqcq"
+export const RazorpayApiSecret  =  "XalGRnwZfrHH7273VR0Ti8mz"
